48ac13ca993430377be9480ab0b46904bd50ca28
Phil Smithies <philsmithies@gmail.com>
2022-01-07 12:11:33 +0100

    web and sanity init

---
+++ web/pages/index.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold text-red-500 underline">Hello world!</h1>
+  );
+}
