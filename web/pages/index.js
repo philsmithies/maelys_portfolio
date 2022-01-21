0853dd8d3516bd10160c2ac88ca0da9d4337d1f8
Phil Smithies <philsmithies@gmail.com>
2022-01-21 09:13:53 +0100

    hero image front page

--- web/pages/index.js
+++ web/pages/index.js
@@ -15,21 +15,9 @@ const Index = () => {
         <Navbar />
       </div>
       <div className="flex flex-col">
-        <div className="w-full h-screen bg-teal-600">
+        <div className="w-full h-screen bg-cover bg-[url('/images/portfolio8.jpg')] top-[200px]">
           <h1 className="text-3xl pl-40 pt-40">Big Heart</h1>
           <h1 className="text-3xl pl-20">Big Drawings</h1>
-          <div>
-            <div
-              style={{ width: "100%", height: "100%", position: "relative" }}
-            >
-              <Image
-                alt="Mountains"
-                src="/images/portfolio8.jpg"
-                layout="fill"
-                objectFit="contain"
-              />
-            </div>
-          </div>
         </div>
         <HomePageGrid />
         <div className="bg-teal-500">
