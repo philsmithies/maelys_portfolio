851dd39cff76359cb557d40c80a8faa235a9c4cf
Phil Smithies <philsmithies@gmail.com>
2022-01-28 11:57:39 +0100

    updated package json for build, blog is connected

--- web/pages/blog.js
+++ web/pages/blog.js
@@ -1,9 +1,39 @@
-const Blog = () => {
+import Link from "next/link";
+import groq from "groq";
+import client from "../client";
+import Navbar from "../components/Navbar";
+
+const Blog = ({ posts }) => {
   return (
     <div>
-      <h1>What is up its a blog</h1>
+      <Navbar />
+      <h1>Welcome to a blog!</h1>
+      {posts.length > 0 &&
+        posts.map(
+          ({ _id, title = "", slug = "", publishedAt = "" }) =>
+            slug && (
+              <li key={_id}>
+                <Link href="/post/[slug]" as={`/post/${slug.current}`}>
+                  <a>{title}</a>
+                </Link>{" "}
+                ({new Date(publishedAt).toDateString()})
+              </li>
+            )
+        )}
     </div>
   );
 };
 
-export default blog;
+export async function getStaticProps() {
+  const posts = await client.fetch(groq`
+      *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
+    `);
+  console.log("posts here", posts);
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Blog;
