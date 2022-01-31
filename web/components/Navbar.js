5b69bb9f40232feb5a0a12dee00ee40e187b5df7
Phil Smithies <philsmithies@gmail.com>
2022-01-31 07:42:42 +0100

    new navbar and intro index

--- web/components/Navbar.js
+++ web/components/Navbar.js
@@ -13,33 +13,42 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="container flex justify-around py-1 mx-auto bg-teal-500 w-screen sticky z-10">
+    <nav className="container flex justify-around py-1 mx-auto w-screen sticky z-10">
       <Link href="/">
-        <div className="hover:cursor-pointer">
-          <Image
-            src="/daisy.png"
-            width={80}
-            height={80}
-            className="self-end justify-self-center"
-          />
+        <div className="hover:cursor-pointer fill-black pb-5 pt-5">
+          <h1 className="text-4xl font-semibold transition duration-150 hover:cursor-pointer">
+            MAËLYS CHA
+          </h1>
         </div>
       </Link>
       <div className="space-x-8 flex items-center font-serif">
-        <a href="/" className="hover:text-teal-600">
+        <a
+          href="/"
+          className="hover:text-teal-600 transition duration-150 border-b-2 border-transparent hover:border-teal-500"
+        >
           Home
         </a>
-        <a href="/about" className="hover:text-teal-600">
-          About Me
+        <a
+          href="/"
+          className="hover:text-teal-600 transition duration-150 border-b-2 border-transparent hover:border-teal-500"
+        >
+          Illustration
         </a>
-        <a href="/blog" className="hover:text-teal-600">
-          Blog
+        <a
+          href="/blog"
+          className="hover:text-teal-600 transition duration-150 border-b-2 border-transparent hover:border-teal-500"
+        >
+          Thoughts
+        </a>
+        <a
+          href="/about"
+          className="hover:text-teal-600 transition duration-150 border-b-2 border-transparent hover:border-teal-500"
+        >
+          About
         </a>
         {/* <a href="" className="hover:text-teal-600">
           Shop
         </a> */}
-        <a href="/contact" className="hover:text-teal-600">
-          Contact
-        </a>
       </div>
     </nav>
   );
--- web/pages/index.js
+++ web/pages/index.js
@@ -8,6 +8,11 @@ import ContactForm from "../components/ContactForm";
 const Index = () => {
   return (
     <div className="flex flex-col">
+      <div className="w-8/12 text-2xl text-center mx-auto p-10">
+        Hi, I’m Maelys. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed pretium nibh a felis porta, vitae faucibus mi varius. Aliquam
+        consectetur suscipit malesuada. ✨
+      </div>
       <div className="w-full h-screen bg-cover bg-[url('/images/portfolio8.jpg')] top-[200px]">
         <h1 className="text-6xl pl-40 pt-40 font-serif text-white">
           Big Heart
