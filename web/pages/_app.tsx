1190ee955d35fc986a99d5f8604b250edf8b5759
Phil Smithies <philsmithies@gmail.com>
2022-03-04 11:32:17 +0100

    fade between page loads

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,11 +1,26 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { motion } from "framer-motion";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
     </Layout>
   );
 }
